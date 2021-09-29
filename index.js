e7fba2497381cc7fb0a89941098ad0092c4bdee5
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-09-29 15:34:44 +0000

    problem

---
+++ index.js
@@ -0,0 +1,5 @@
+/*
+Given two stings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+*/
